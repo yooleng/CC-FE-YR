99cd95b51582f6e169aaaedcfa988b8410e450cb
yrk721 <yoorae0721@naver.com>
2023-02-08 08:19:55 +0900

    Create emotion.js

---
+++ class_quiz/pages/01/emotion.js
@@ -0,0 +1,235 @@
+// import styled from "@emotion/styled";
+
+// export const Box = styled.div`
+//   position: relative;
+//   display: flex;
+//   width: 640px;
+//   height: 1138px;
+//   background: white;
+//   border: solid #dcdcdc;
+// `;
+
+// export const SearchIcon = styled.div`
+//   position: absolute;
+//   display: flex;
+//   justify-content: flex-end;
+//   margin-top: 50pt;
+//   margin-right: 48pt;
+//   width: 600px;
+//   height: 32px;
+// `;
+
+// export const TitleBox = styled.div`
+//   display: flex;
+//   justify-content: flex-end;
+//   position: absolute;
+//   width: 600px;
+//   height: 64px;
+//   margin-top: 150px;
+// `;
+
+// export const Title = styled.div`
+//   margin-right: 120px;
+//   width: 212px;
+//   height: 64px;
+//   font-family: "Noto Sans KR";
+//   font-style: normal;
+//   font-weight: 700;
+//   font-size: 40px;
+//   line-height: 58px;
+//   color: #000000;
+// `;
+
+// export const UserProfile = styled.div`
+//   display: flex;
+//   align-items: center;
+//   width: 200px;
+//   height: 60px;
+// `;
+
+// export const UserPicture = styled.div`
+//   display: flex;
+//   width: 60px;
+//   height: 60px;
+// `;
+
+// export const UserID = styled.div`
+//   display: flex;
+//   width: 67px;
+//   height: 35px;
+//   width: 67px;
+//   height: 35px;
+//   margin-left: 15px;
+//   margin-right: 15px;
+//   font-family: "Noto Sans KR";
+//   font-style: normal;
+//   font-weight: 700;
+//   font-size: 24px;
+//   line-height: 35px;
+//   text-align: center;
+//   color: #000000;
+// `;
+
+// export const NoticeBox = styled.div`
+//   display: flex;
+//   justify-content: space-around;
+//   align-items: center;
+//   width: 500px;
+//   height: 50px;
+//   margin-top: 250px;
+//   margin-left: 40px;
+// `;
+
+// export const Notice = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   width: 111px;
+//   height: 43px;
+//   font-family: "Noto Sans KR";
+//   font-style: normal;
+//   font-weight: 700;
+//   font-size: 30px;
+//   line-height: 43px;
+//   color: #cacaca;
+// `;
+
+// export const Event = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   width: 83px;
+//   height: 43px;
+//   font-family: "Noto Sans KR";
+//   font-style: normal;
+//   font-weight: 700;
+//   font-size: 30px;
+//   line-height: 43px;
+//   color: #cacaca;
+// `;
+
+// export const FAQ = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   width: 58px;
+//   height: 43px;
+//   border-bottom: 2px solid #ff1b6d;
+//   font-family: "Noto Sans KR";
+//   font-style: normal;
+//   font-weight: 700;
+//   font-size: 30px;
+//   line-height: 43px;
+//   color: #ff1b6d;
+// `;
+
+// export const QnA = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   width: 65px;
+//   height: 43px;
+//   font-family: "Noto Sans KR";
+//   font-style: normal;
+//   font-weight: 700;
+//   font-size: 30px;
+//   line-height: 43px;
+//   color: #cacaca;
+// `;
+
+// export const ContentsBox = styled.div`
+//   position: absolute;
+//   display: flex;
+//   margin-top: 347px;
+//   width: 640px;
+//   height: 791px;
+// `;
+
+// export const QuestionsListBox = styled.div`
+//   position: absolute;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-around;
+//   width: 640px;
+//   height: 635px;
+//   padding-top: 30px;
+//   padding-bottom: 30px;
+//   border-top: 1px solid #cacaca;
+// `;
+
+// export const QuestionsList = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   margin-left: 65px;
+//   width: 510px;
+//   height: 60px;
+//   font-family: "Noto Sans KR";
+//   font-style: normal;
+//   font-weight: 400;
+//   font-size: 24px;
+//   line-height: 35px;
+//   color: #000000;
+// `;
+
+// export const QuestionsNumber = styled.div`
+//   font-family: "Noto Sans KR";
+//   font-style: normal;
+//   font-weight: 400;
+//   font-size: 18px;
+//   line-height: 26px;
+//   /* identical to box height */
+
+//   color: #adadad;
+// `;
+
+// export const ToggleBox = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: flex-end;
+//   width: 100px;
+//   height: 50px;
+// `;
+
+// export const BottomBox = styled.div`
+//   box-sizing: border-box;
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-around;
+//   width: 640px;
+//   height: 96px;
+//   padding: 10px;
+//   border-top: 1px solid #dcdcdc;
+//   margin-top: 695px;
+//   /* border: 2px solid #ff1b6d; */
+// `;
+
+// export const MenuBox = styled.div`
+//   box-sizing: border-box;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   width: 70px;
+//   height: 70px;
+//   font-family: "Noto Sans KR";
+//   font-style: normal;
+//   font-weight: 400;
+//   font-size: 16px;
+//   line-height: 23px;
+//   text-align: center;
+//   color: #adadad;
+// `;
+
+// export const MenuText = styled.div`
+//   box-sizing: border-box;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   width: 70px;
+//   height: 20px;
+//   font-family: "Noto Sans KR";
+//   font-style: normal;
+//   font-weight: 400;
+//   font-size: 16px;
+//   line-height: 23px;
+//   text-align: center;
+
+//   color: #adadad;
+// `;
