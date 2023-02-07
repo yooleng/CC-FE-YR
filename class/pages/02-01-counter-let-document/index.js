8ae2f409af0ac20e739b13587eeb174e289ff4e2
yrk721 <yoorae0721@naver.com>
2023-02-08 04:53:45 +0900

    class를 활용한 counter

---
+++ class/pages/02-01-counter-let-document/index.js
@@ -0,0 +1,21 @@
+// 기존 방식 - class
+
+export default function CounterLetDocumentPage() {
+  function onClickCountUp() {
+    const count = Number(document.getElementById("count").innerText) + 1;
+    document.getElementById("count").innerText = count;
+  }
+
+  function onClickCountDown() {
+    const count = Number(document.getElementById("count").innerText) - 1;
+    document.getElementById("count").innerText = count;
+  }
+
+  return (
+    <div>
+      <div id="count">0</div>
+      <button onClick={onClickCountUp}>카운트 올리기</button>
+      <button onClick={onClickCountDown}>카운트 내리기</button>
+    </div>
+  );
+}
