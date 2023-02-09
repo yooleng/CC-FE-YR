68e248e49af9927e3ed872ebd8d44c0d03249ecb
yrk721 <yoorae0721@naver.com>
2023-02-09 22:29:18 +0900

    모든 페이지에서 useMutation을 사용하기 위한 설정

--- class/pages/_app.js
+++ class/pages/_app.js
@@ -1,3 +1,19 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+function App({ Component, pageProps }) {
+  // 모든 페이지에서 useMutation을 사용하기 위한 설정
+  // ApolloClient 공식 홈페이지에 가면 설정방법 메뉴얼이 있음
+
+  const client = new ApolloClient({
+    uri: "http://practice.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(), // 나중에 할 것
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
+
+export default App;
