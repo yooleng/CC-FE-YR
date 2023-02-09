2a67d1b958292041931c288b58de9434cff8f10c
yrk721 <yoorae0721@naver.com>
2023-02-10 04:59:20 +0900

    #day04_quiz_04-1_REST-API 요청과 비동기 처리

---
+++ class_quiz/pages/04-1/index.js
@@ -0,0 +1,19 @@
+// ----------------------------------------------------------------------------------
+// REST-API 요청과 비동기 처리
+// ----------------------------------------------------------------------------------
+
+import axios from "axios";
+import { useState } from "react";
+
+export default function QuizPage1() {
+  const [user, setUser] = useState("");
+
+  const onClickAsync = async () => {
+    const result = await axios.get("https://koreanjson.com/users");
+    console.log(result);
+    console.log(result.data);
+    setUser(result.data);
+  };
+
+  return <button onClick={onClickAsync}>REST-API 요청하기</button>;
+}
