3c9deba4201a04b4657e75362ad0e154d9991dfa
yrk721 <yoorae0721@naver.com>
2023-02-14 06:04:37 +0900

    BoardDetail.styles - page의 emotion 부분

---
+++ class_quiz/src/components/units/board/detail/BoardDetail.styles.js
@@ -0,0 +1,23 @@
+// ----------------------------------------------------------------------------------
+// BoardDetail.styles - page의 emotion 부분
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+export const EContentstBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  height: 300px;
+  padding: 20px;
+  border: 2px solid gray;
+`;
+
+export const ETextBox = styled.div`
+  width: 250px;
+  height: 30px;
+  padding: 10px;
+  border: 1px solid gray;
+`;
