dffd941e27721b13370d4b1d382ec4debeaa77b0
yrk721 <yoorae0721@naver.com>
2023-02-14 00:44:36 +0900

    BoardWrite.styles - page의 emotion 부분

---
+++ class/src/components/units/board/write/BoardWrite.styles.js
@@ -0,0 +1,13 @@
+// ----------------------------------------------------------------------------------
+// BoardWrite.styles - page의 emotion 부분
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+export const RedInput = styled.input`
+  border-color: red;
+`;
+
+export const BlueButton = styled.button`
+  background-color: blue;
+`;
