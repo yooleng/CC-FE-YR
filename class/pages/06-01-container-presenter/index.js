1945faec71b831b94d22f87bd4129686158f33c5
yrk721 <yoorae0721@naver.com>
2023-02-14 00:43:59 +0900

    [day6] 06-01-container-presenter 파일분리

---
+++ class/pages/06-01-container-presenter/index.js
@@ -0,0 +1,18 @@
+// ----------------------------------------------------------------------------------
+// 06-01-container-presenter
+//  -> 04-04. graphql-mutation-input 파일분리
+// ----------------------------------------------------------------------------------
+
+import BoardWrite from "@/src/components/units/board/write/BoardWrite.container";
+
+export default function GraphqlMutationPage() {
+  // javascript 영역
+
+  return (
+    // html 영역
+    <>
+      <div>### page ###</div>
+      <BoardWrite />
+    </>
+  );
+}
