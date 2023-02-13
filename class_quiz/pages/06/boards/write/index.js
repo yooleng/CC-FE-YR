df03c01220998f552306aaff2f0343864f236d9d
yrk721 <yoorae0721@naver.com>
2023-02-14 06:02:17 +0900

    [# day06] 폴더구조 체계화(Container/Presenter 패턴) - routing (write)

---
+++ class_quiz/pages/06/boards/write/index.js
@@ -0,0 +1,16 @@
+// ----------------------------------------------------------------------------------
+// day 6 : 컴포넌트 디자인 패턴 & 리액트의 단방향 흐름
+// ----------------------------------------------------------------------------------
+// 폴더구조 체계화(Container/Presenter 패턴) - routing (write)
+// ----------------------------------------------------------------------------------
+
+import BoardWrite from "@/src/components/units/board/write/BoardWrite.container";
+
+export default function QuizPage6() {
+  // javascript 영역
+
+  return (
+    // html 영역
+    <BoardWrite />
+  );
+}
