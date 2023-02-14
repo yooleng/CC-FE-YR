978545a599f92b1053ee66039f8329af94d93d46
yrk721 <yoorae0721@naver.com>
2023-02-15 07:06:21 +0900

    [# day07] 07-01. setState

---
+++ class/pages/07-01-setState/index.js
@@ -0,0 +1,13 @@
+// ----------------------------------------------------------------------------------
+// 07-01. setState
+// ----------------------------------------------------------------------------------
+
+import BoardWrite from "@/src/components/units/board/07-write/BoardWrite.container";
+
+export default function GraphqlMutationPage() {
+  return (
+    <>
+      <BoardWrite />
+    </>
+  );
+}
