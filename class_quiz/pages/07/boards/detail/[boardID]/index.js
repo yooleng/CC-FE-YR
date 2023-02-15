9f3ad8371553e79dd64f6a5d74008aabf64b72ef
yrk721 <yoorae0721@naver.com>
2023-02-15 09:05:53 +0900

    Create index.js

---
+++ class_quiz/pages/07/boards/detail/[boardID]/index.js
@@ -0,0 +1,16 @@
+// ----------------------------------------------------------------------------------
+// day 7 : 컴포넌트 디자인 패턴 & 리액트의 단방향 흐름
+// ----------------------------------------------------------------------------------
+// 폴더구조 체계화(Container/Presenter 패턴) - routed (detail)
+// ----------------------------------------------------------------------------------
+
+import BoardDetail from "@/src/components/units/board/detail/BoardDetail.container";
+
+export default function QuizPage6() {
+  // javascript 영역
+
+  return (
+    // html 영역
+    <BoardDetail />
+  );
+}
