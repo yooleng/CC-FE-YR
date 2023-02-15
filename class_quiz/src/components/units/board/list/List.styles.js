7063dfca677af055b762b456395f75189a996d34
yrk721 <yoorae0721@naver.com>
2023-02-15 09:05:50 +0900

    List.styles - page의 emotion 부분

---
+++ class_quiz/src/components/units/board/list/List.styles.js
@@ -0,0 +1,52 @@
+// ----------------------------------------------------------------------------------
+// 07. List.styles - page의 emotion 부분
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+export const Row = styled.div`
+  display: flex;
+`;
+
+export const Column = styled.div`
+  /* width: ${(props) => props.w};
+  background-color: ${(props) => props.c}; */
+  width: 20%;
+  margin-bottom: 10px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 20px 0 30px 0;
+`;
+
+export const HeaderTitle = styled.div`
+  text-align: start;
+  font-weight: bold;
+  width: 20%;
+`;
+
+export const HeaderCheckBox = styled.div`
+  width: 5%;
+`;
+
+export const Invisible = styled.div`
+  opacity: 0;
+  width: 5%;
+`;
+
+export const HeaderDelete = styled.div`
+  opacity: 0;
+`;
+
+export const DeleteButton = styled.div`
+  // presenter에서 emotion으로 props 받기
+  border: 1px solid ${(props) => props.c};
+  border-radius: 5px;
+  padding: 5px;
+  justify-content: center;
+  text-align: center;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
