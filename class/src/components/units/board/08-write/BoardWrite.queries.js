6b8a062db2745f61f39483a352024fcb0ee88a30
yrk721 <yoorae0721@naver.com>
2023-02-16 01:56:49 +0900

    08. BoardWrite.queries - graphql query 부분

---
+++ class/src/components/units/board/08-write/BoardWrite.queries.js
@@ -0,0 +1,38 @@
+// ----------------------------------------------------------------------------------
+// 08. BoardWrite.queries - graphql query 부분
+// ----------------------------------------------------------------------------------
+
+import { gql } from "@apollo/client";
+
+// graphql 쿼리문
+export const CREATE_BOARD = gql`
+  mutation createBoard($writer: String, $title: String, $contents: String) {
+    # 변수의 타입을 적는 곳
+    createBoard(writer: $writer, title: $title, contents: $contents) {
+      # 실제 우리가 전달할 변수 적는 곳
+      _id
+      number
+      message
+    }
+  }
+`;
+
+export const UPDATE_BOARD = gql`
+  mutation updateBoard(
+    $number: Int
+    $writer: String
+    $title: String
+    $contents: String
+  ) {
+    updateBoard(
+      number: $number
+      writer: $writer
+      title: $title
+      contents: $contents
+    ) {
+      _id
+      number
+      message
+    }
+  }
+`;
