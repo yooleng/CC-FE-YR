9508957a52e79b0e9834b239b5bca5c44fd66520
yrk721 <yoorae0721@naver.com>
2023-02-16 01:55:16 +0900

    08-04. board-component-edit

---
+++ class/pages/08-04-board-component-edit/index.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------------------
+// 08-04. board-component-edit
+// ----------------------------------------------------------------------------------
+
+import BoardComponent from "@/src/components/units/board/08-board-component";
+
+export default function BoardEditPage() {
+  return <BoardComponent isEdit={true} />;
+}
