7a74d64e453853c75a5b751b62944563ae9edc88
yrk721 <yoorae0721@naver.com>
2023-02-17 04:14:13 +0900

    product.presenter - page의 html 부분 - defaultValue 추가

--- class_quiz/src/components/units/board/08-products/Products.presenter.js
+++ class_quiz/src/components/units/board/08-products/Products.presenter.js
@@ -1,5 +1,5 @@
 // ----------------------------------------------------------------------------------
-// day 8 : 컴포넌트 재사용성과 수정 프로세스
+// day 8 ~ 9 : 컴포넌트 재사용성과 수정 프로세스
 // ----------------------------------------------------------------------------------
 // product.presenter - page의 html 부분
 // ----------------------------------------------------------------------------------
@@ -13,21 +13,41 @@ export default function ProductsWriteComponentUI(props) {
 
       <S.EContentstBox>
         <S.EInputBox>
-          판매자: <input type="text" onChange={props.onChangeSeller} />
+          판매자:
+          <input
+            type="text"
+            onChange={props.onChangeSeller}
+            defaultValue={props.data?.fetchProduct.seller}
+          />
         </S.EInputBox>
         <S.EInputBox>
-          상품명: <input type="text" onChange={props.onChangeName} />
+          상품명:
+          <input
+            type="text"
+            onChange={props.onChangeName}
+            defaultValue={props.data?.fetchProduct.name}
+          />
         </S.EInputBox>
         <S.EInputBox>
-          상품내용: <input type="text" onChange={props.onChangeDetail} />
+          상품내용:
+          <input
+            type="text"
+            onChange={props.onChangeDetail}
+            defaultValue={props.data?.fetchProduct.detail}
+          />
         </S.EInputBox>
         <S.EInputBox>
-          상품가격: <input type="text" onChange={props.onChangePrice} />
+          상품가격:
+          <input
+            type="text"
+            onChange={props.onChangePrice}
+            defaultValue={props.data?.fetchProduct.price}
+          />
         </S.EInputBox>
 
         {/* presenter에서 emotion으로 props 던지기 */}
         <S.EButton
-          ccc={props.myColor}
+          myColor={props.myColor}
           // 바인딩 로직 변경 (등록하기/수정하기 조건 추가)
           onClick={props.isEdit ? props.onClickUpdate : props.onClickSubmit}
         >
--- class_quiz/src/components/units/board/08-products/Products.queries.js
+++ class_quiz/src/components/units/board/08-products/Products.queries.js
@@ -1,5 +1,5 @@
 // ----------------------------------------------------------------------------------
-// day 8 : 컴포넌트 재사용성과 수정 프로세스
+// day 8 ~ 9 : 컴포넌트 재사용성과 수정 프로세스
 // ----------------------------------------------------------------------------------
 // product.queries - graphql query 부분
 // ----------------------------------------------------------------------------------
@@ -33,31 +33,3 @@ export const UPDATE_PRODUCT = gql`
     }
   }
 `;
-
-// Q. UPDATE_PRODUCT 쿼리문 질문
-// docs를 보면, UpdateProductInput가 하나의 타입이고, name, detail, price 세 개의 필드를 갖는다고 나와있는데,
-// 아래처럼 써도 되는건지 궁금합니다.
-
-// + updateProduct 이후 "상품이 정상적으로 수정되었습니다."라는 message만 뜨고
-// 나머지 데이터가 모두 null값으로 반환되는데, 이 오류를 해결하지 못했습니다.
-// 어디를 수정해야 할까요...
-
-// -----------------------------------------
-
-// export const UPDATE_PRODUCT = gql`
-//   mutation updateProduct(
-//     $productId: ID
-//     $name: String
-//     $detail: String
-//     $price: Int
-//   ) {
-//     updateProduct(
-//       productId: $productId
-//       updateProductInput: { name: $name, detail: $detail, price: $price }
-//     ) {
-//       _id
-//       number
-//       message
-//     }
-//   }
-// `;
