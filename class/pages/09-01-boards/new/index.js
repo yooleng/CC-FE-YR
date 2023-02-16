d7631a8caa67599e061491b33cd1d84b94e47d86
yrk721 <yoorae0721@naver.com>
2023-02-16 16:56:10 +0900

    [# day 9] 09-01. boards - new (등록 페이지)

---
+++ class/pages/09-01-boards/new/index.js
@@ -0,0 +1,13 @@
+// ----------------------------------------------------------------------------------
+// 09-01. boards - new (등록 페이지)
+// ----------------------------------------------------------------------------------
+
+import BoardWriteComponent from "@/src/components/units/board/09-write/BoardWrite.container";
+
+export default function BoardsNewPage() {
+  return (
+    <>
+      <BoardWriteComponent isEdit={false} />
+    </>
+  );
+}
