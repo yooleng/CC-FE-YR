b9e865f58b62345e4700d4c8e15de233b5ef01b5
yrk721 <yoorae0721@naver.com>
2023-02-20 02:51:08 +0900

    10-03. graphql-codegen-mutation - useMutation 타입 지정

---
+++ class/pages/10-03-graphql-codegen-mutation/index.tsx
@@ -0,0 +1,76 @@
+// ----------------------------------------------------------------------------------
+// 10-03. graphql-codegen-mutation - useMutation 타입 지정
+// ----------------------------------------------------------------------------------
+
+import { useMutation } from "@apollo/client";
+import { gql } from "@apollo/client";
+import { ChangeEvent, useState } from "react";
+import {
+  IMutation,
+  IMutationCreateBoardArgs,
+} from "../../src/commons/types/generated/types";
+
+// graphql 쿼리문
+const CREATE_BOARD = gql`
+  mutation createBoard($writer: String, $title: String, $contents: String) {
+    # 변수의 타입을 적는 곳
+    createBoard(writer: $writer, title: $title, contents: $contents) {
+      # 실제 우리가 전달할 변수 적는 곳
+      _id
+      number
+      message
+    }
+  }
+`;
+
+export default function GraphqlMutationPage() {
+  // 변수를 state에 저장하기
+  const [writer, setWriter] = useState<string>("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  // const [myfunction] = useMutation<RESULT타입, VARIABLES(인자)타입)>(CREATE_BOARD);
+  const [myfunction] = useMutation<
+    Pick<IMutation, "createBoard">,
+    IMutationCreateBoardArgs
+  >(CREATE_BOARD);
+
+  const onClickSubmit = async () => {
+    const result = await myfunction({
+      variables: {
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+
+    // data의 메세지를 alert창에 띄우기
+    alert(result.data?.createBoard?.message);
+  };
+
+  // 화살표 함수로 이벤트 핸들러 함수 작성 후 html에 바인딩
+  const onChangeWriter = (event: ChangeEvent<HTMLInputElement>) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event: ChangeEvent<HTMLInputElement>) => {
+    setContents(event.target.value);
+  };
+
+  return (
+    <>
+      작성자: <input type="text" onChange={onChangeWriter} />
+      <br />
+      제목: <input type="text" onChange={onChangeTitle} />
+      <br />
+      내용: <input type="text" onChange={onChangeContents} />
+      <br />
+      <button onClick={onClickSubmit}>GRAPHQL-API(동기) 요청하기</button>
+    </>
+  );
+}
