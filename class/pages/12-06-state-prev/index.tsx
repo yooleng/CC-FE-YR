87d0e75055c7f40e8cb5ed661aa43ac21ea360ff
yrk721 <yoorae0721@naver.com>
2023-02-22 07:32:42 +0900

    12-06. state-prev

---
+++ class/pages/12-06-state-prev/index.tsx
@@ -0,0 +1,32 @@
+// ----------------------------------------------------------------------------------
+// 12-06. state-prev
+// ----------------------------------------------------------------------------------
+
+import React, { useState } from "react";
+
+export default function CounterStatePage() {
+  const [count, setCount] = useState(0); // 구조분해할당
+
+  function onClickCountUp() {
+    setCount((prevState) => prevState + 1);
+    setCount((prevState) => prevState + 1);
+    setCount((prevState) => prevState + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+
+    console.log(count);
+  }
+
+  function onClickCountDown() {
+    setCount((prevState) => prevState - 1);
+  }
+
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={onClickCountUp}>카운트 올리기</button>
+      <button onClick={onClickCountDown}>카운트 내리기</button>
+    </div>
+  );
+}
