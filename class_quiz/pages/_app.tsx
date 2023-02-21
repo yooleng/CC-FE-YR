e9a7e42417e9bdf2d32dd0a57bc932113c5889ba
yrk721 <yoorae0721@naver.com>
2023-02-21 20:52:50 +0900

    update _app (js -> tsx)

--- class_quiz/pages/_app.tsx
+++ class_quiz/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { AppProps } from "next/app";
 
-function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   // 모든 페이지에서 useMutation을 사용하기 위한 설정
   // ApolloClient 공식 홈페이지에 가면 설정방법 메뉴얼이 있음
 
@@ -11,9 +12,10 @@ function App({ Component, pageProps }) {
 
   return (
     <ApolloProvider client={client}>
+      <div>### app.js ###</div>
       <Component {...pageProps} />
     </ApolloProvider>
   );
 }
 
-export default App;
+export default MyApp;
