d522e4187dc3d7bdaf6a2cca23a83f4c740a51a8
yrk721 <yoorae0721@naver.com>
2023-02-22 23:38:42 +0900

    [#day 13 ] 13-01. props-children

---
+++ class/pages/13-01-props-children/index.tsx
@@ -0,0 +1,15 @@
+// ----------------------------------------------------------------------------------
+// 13-01. props-children
+// ----------------------------------------------------------------------------------
+
+import Layout from "../../src/components/units/13-props-children";
+
+export default function PropsChildrenPage() {
+  return (
+    <Layout school="다람쥐초등학교">
+      {/* <input type="text" /> */}
+      {/* <button>클릭해주세요!</button> */}
+      <div>게시글 목록 페이지</div>
+    </Layout>
+  );
+}
