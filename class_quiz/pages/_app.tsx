91afb1c072a1a7726496eda95439dfe7f8423b16
yrk721 <yoorae0721@naver.com>
2023-02-24 11:23:39 +0900

    Update _app.tsx

--- class_quiz/pages/_app.tsx
+++ class_quiz/pages/_app.tsx
@@ -1,20 +1,23 @@
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import ApolloSetting from "../src/components/commons/apollo";
+import Layout from "../src/components/commons/layout";
 import { AppProps } from "next/app";
+import { Global } from "@emotion/react";
+import { globalStyles } from "../src/commons/styles/globalStyles";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // 모든 페이지에서 useMutation을 사용하기 위한 설정
   // ApolloClient 공식 홈페이지에 가면 설정방법 메뉴얼이 있음
 
-  const client = new ApolloClient({
-    uri: "http://practice.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(), // 나중에 할 것
-  });
-
   return (
-    <ApolloProvider client={client}>
-      <div>### app.js ###</div>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <ApolloSetting>
+      <>
+        <Global styles={globalStyles} />
+        <div>### app.js ###</div>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
+    </ApolloSetting>
   );
 }
 
