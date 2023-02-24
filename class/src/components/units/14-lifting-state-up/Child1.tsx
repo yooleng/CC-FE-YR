1a43e95fd3e802b6671886b7a3c4ecb2042aa8c0
yrk721 <yoorae0721@naver.com>
2023-02-24 11:16:10 +0900

    14-04. lifting-state-up - Child 1

---
+++ class/src/components/units/14-lifting-state-up/Child1.tsx
@@ -0,0 +1,14 @@
+// ----------------------------------------------------------------------------------
+// 14-04. lifting-state-up - Child 1
+// ----------------------------------------------------------------------------------
+
+export default function Child1(props: any) {
+  // 부모의 state 조작방법 2
+  const onClick = () => [props.setCount((prev: number) => prev + 1)];
+  return (
+    <>
+      <div>자식 1의 카운트: {props.count}</div>
+      <button onClick={onClick}>카운트 올리기!</button>
+    </>
+  );
+}
