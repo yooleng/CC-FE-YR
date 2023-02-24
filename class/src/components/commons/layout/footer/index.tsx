b08252d442c321339d48624f48b2d0233a8534ae
yrk721 <yoorae0721@naver.com>
2023-02-24 11:14:37 +0900

    create Layout - footer component

---
+++ class/src/components/commons/layout/footer/index.tsx
@@ -0,0 +1,14 @@
+// ----------------------------------------------------------------------------------
+// Layout - footer
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  height: 50px;
+  background-color: gray;
+`;
+
+export default function LayoutFooter() {
+  return <Wrapper>여기는 푸터입니다</Wrapper>;
+}
