3768e2c71b0b8ef8533da9678d901200273c1fa4
yrk721 <yoorae0721@naver.com>
2023-02-24 11:20:30 +0900

    13. create Layout -header component

---
+++ class_quiz/src/components/commons/layout/header/index.tsx
@@ -0,0 +1,14 @@
+// ----------------------------------------------------------------------------------
+// day 13. Layout - header
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  height: 50px;
+  background-color: #d82c2c;
+`;
+
+export default function LayoutHeader() {
+  return <Wrapper>헤더영역</Wrapper>;
+}
