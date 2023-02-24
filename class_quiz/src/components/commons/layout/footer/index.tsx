aa33d07192b444a6e6074f11ba51d78c04c47b2d
yrk721 <yoorae0721@naver.com>
2023-02-24 11:21:01 +0900

    13. create Layout - footer component

---
+++ class_quiz/src/components/commons/layout/footer/index.tsx
@@ -0,0 +1,14 @@
+// ----------------------------------------------------------------------------------
+// day 13. Layout - footer
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  height: 50px;
+  background-color: green;
+`;
+
+export default function LayoutFooter() {
+  return <Wrapper>푸터영역</Wrapper>;
+}
