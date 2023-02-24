aa6511af15ba7f2323573cbce21aa25196a8ad66
yrk721 <yoorae0721@naver.com>
2023-02-24 11:13:55 +0900

    create Layout - header component

---
+++ class/src/components/commons/layout/header/index.tsx
@@ -0,0 +1,14 @@
+// ----------------------------------------------------------------------------------
+// Layout - header
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  height: 50px;
+  background-color: skyblue;
+`;
+
+export default function LayoutHeader() {
+  return <Wrapper>여기는 헤더입니다</Wrapper>;
+}
