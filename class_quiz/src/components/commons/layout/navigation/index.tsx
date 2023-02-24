c4f598d51bbe7119e45fdf6b1e818ded454f0090
yrk721 <yoorae0721@naver.com>
2023-02-24 11:20:53 +0900

    13. create Layout - navigation component

---
+++ class_quiz/src/components/commons/layout/navigation/index.tsx
@@ -0,0 +1,14 @@
+// ----------------------------------------------------------------------------------
+// day 13. Layout - navigation
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  height: 50px;
+  background-color: #ee9f0c;
+`;
+
+export default function LayoutNavigation() {
+  return <Wrapper>네비게이션 영역</Wrapper>;
+}
