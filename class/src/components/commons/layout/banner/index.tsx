01772427db1464ca4e3994d4dab26682e23177f1
yrk721 <yoorae0721@naver.com>
2023-02-24 11:14:19 +0900

    create Layout - banner component

---
+++ class/src/components/commons/layout/banner/index.tsx
@@ -0,0 +1,14 @@
+// ----------------------------------------------------------------------------------
+// Layout - banner
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  height: 100px;
+  background-color: yellow;
+`;
+
+export default function LayoutBanner() {
+  return <Wrapper>여기는 배너입니다</Wrapper>;
+}
