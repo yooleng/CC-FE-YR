ec163fa59e802db43b6a1087ffb0a728f0e13fc4
yrk721 <yoorae0721@naver.com>
2023-02-24 11:13:45 +0900

    create Layout component

---
+++ class/src/components/commons/layout/index.tsx
@@ -0,0 +1,36 @@
+// ----------------------------------------------------------------------------------
+// Layout
+// ----------------------------------------------------------------------------------
+
+import { useRouter } from "next/router";
+import LayoutBanner from "./banner";
+import LayoutFooter from "./footer";
+import LayoutHeader from "./header";
+import LayoutNavigation from "./navigation";
+
+const HIDDEN_HEADERS = ["/12-02-library-star/01"];
+
+interface ILayoutProps {
+  children: JSX.Element;
+}
+
+export default function Layout(props: ILayoutProps) {
+  const router = useRouter();
+  console.log(router);
+  console.log(router.asPath);
+
+  const isHiddenHeader = HIDDEN_HEADERS.includes(router.asPath);
+
+  return (
+    <>
+      {!isHiddenHeader && <LayoutHeader />}
+      <LayoutBanner />
+      <LayoutNavigation />
+      <div style={{ height: "500px", display: "flex" }}>
+        <div style={{ width: "30%", backgroundColor: "orange" }}>사이드바</div>
+        <div style={{ width: "70%" }}>{props.children}</div>
+      </div>
+      <LayoutFooter />
+    </>
+  );
+}
