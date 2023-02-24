87ba9a17894cf50fb3e781b71451826da5a016bf
yrk721 <yoorae0721@naver.com>
2023-02-24 11:13:08 +0900

    Apollo (graphql 설정)

---
+++ class/src/components/commons/apollo/index.tsx
@@ -0,0 +1,23 @@
+// ----------------------------------------------------------------------------------
+// Apollo (graphql 설정)
+// ----------------------------------------------------------------------------------
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+interface IApolloSettingProps {
+  children: JSX.Element;
+}
+
+export default function ApolloSetting(props: IApolloSettingProps) {
+  const client = new ApolloClient({
+    uri: "http://backendonline.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(), // 나중에 할 것
+  });
+
+  // prettier-ignore
+  return (
+      <ApolloProvider client={client}>
+        {props.children}
+      </ApolloProvider>
+  );
+}
