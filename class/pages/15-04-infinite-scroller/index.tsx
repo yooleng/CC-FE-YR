2e626e861f51ee299aca8917fab8e5cb1b5cc1d3
yrk721 <yoorae0721@naver.com>
2023-02-27 05:03:58 +0900

    15-04. infinite-scroller (무한스크롤)

---
+++ class/pages/15-04-infinite-scroller/index.tsx
@@ -0,0 +1,68 @@
+// ----------------------------------------------------------------------------------
+// 15-04. infinite-scroller
+// ----------------------------------------------------------------------------------
+
+import { useQuery, gql } from "@apollo/client";
+import InfiniteScroll from "react-infinite-scroller";
+import {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../src/commons/types/generated/types";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutedPage() {
+  const { data, fetchMore } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
+
+  console.log("data", data?.fetchBoards);
+
+  const onLoadMore = () => {
+    if (data === undefined) return;
+
+    void fetchMore({
+      variables: { page: Math.ceil(data?.fetchBoards.length / 10) + 1 },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (fetchMoreResult.fetchBoards === undefined) {
+          return {
+            fetchBoards: [...prev.fetchBoards],
+          };
+        }
+        return {
+          fetchBoards: [...prev.fetchBoards, ...fetchMoreResult.fetchBoards],
+        };
+      },
+    });
+  };
+
+  return (
+    <>
+      <div style={{ height: "500px", overflow: "auto" }}>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={onLoadMore}
+          hasMore={true}
+          useWindow={false}
+        >
+          {data?.fetchBoards.map((el) => (
+            <div key={el._id}>
+              <span style={{ margin: "10px" }}>{el.writer} </span>
+              <span style={{ margin: "10px" }}>{el.title}</span>
+            </div>
+          )) ?? <div></div>}
+        </InfiniteScroll>
+      </div>
+    </>
+  );
+}
