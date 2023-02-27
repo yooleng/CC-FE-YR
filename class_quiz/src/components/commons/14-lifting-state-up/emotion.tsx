7738f5ed37774eee95e5f84ec682239f335cedc4
yrk721 <yoorae0721@naver.com>
2023-02-28 04:07:25 +0900

    Create emotion.tsx

---
+++ class_quiz/src/components/commons/14-lifting-state-up/emotion.tsx
@@ -0,0 +1,15 @@
+// ----------------------------------------------------------------------------------
+// day 14. Pagination - emotion
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+export const CurrentPage = styled.span`
+  margin: 0px 10px;
+`;
+
+// // presenter에서 emotion으로 props 받기
+// export const BlueButton = styled.button`
+//   font-size: ${(props) => props.aaa};
+//   background-color: ${(props) => (props.ccc ? "yellow" : "default")};
+// `;
