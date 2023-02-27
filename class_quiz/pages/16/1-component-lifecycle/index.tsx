c9ddd317562ffbc88052cfe10edb325f6219da7c
yrk721 <yoorae0721@naver.com>
2023-02-28 07:28:31 +0900

    [#day 16 ] 16. 1-component-lifecycle (컴포넌트 생명주기)

---
+++ class_quiz/pages/16/1-component-lifecycle/index.tsx
@@ -0,0 +1,42 @@
+// ----------------------------------------------------------------------------------
+// day 16. 1-component-lifecycle (컴포넌트 생명주기)
+// ----------------------------------------------------------------------------------
+
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function ComponentLifecyclePage() {
+  const [isChange, setIsChange] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log("Rendered!");
+    alert("Rendered!");
+    return () => {
+      console.log("Bye!!");
+      alert("Bye!!");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("Changed!!");
+    alert("Changed!!");
+  });
+
+  const onClickIsChange = () => {
+    setIsChange(true);
+  };
+  console.log(isChange);
+
+  const onClickMove = () => {
+    // router.push("/");
+    router.push("./2-class-to-function/02-after");
+  };
+
+  return (
+    <>
+      <button onClick={onClickIsChange}>변경</button>
+      <button onClick={onClickMove}>이동</button>
+    </>
+  );
+}
