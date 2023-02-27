9554c9b22a031fba4f89da54fbe1c0991c09f069
yrk721 <yoorae0721@naver.com>
2023-02-28 07:29:10 +0900

    16. 2-class-to-function - after (클래스 컴포넌트를 함수형 컴포넌트로 변경하기)

---
+++ class_quiz/pages/16/2-class-to-function/02-after.tsx
@@ -0,0 +1,41 @@
+// ----------------------------------------------------------------------------------
+// day 16. 2-class-to-function - after (클래스 컴포넌트를 함수형 컴포넌트로 변경하기)
+// ----------------------------------------------------------------------------------
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+export default function MyComponent() {
+  const [count, setCount] = useState(0);
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log("컴포넌트가 마운트됐습니다~");
+
+    return () => {
+      console.log("컴포넌트가 제거됩니다~");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("컴포넌트가 변경됐습니다~");
+  });
+
+  const onClickCounter = () => {
+    console.log("count", count);
+    setCount((prev) => prev + 1);
+  };
+
+  const onClickMove = () => {
+    // void router.push("/");
+    void router.push("../3-open-api");
+  };
+
+  return (
+    <>
+      <div>카운트: {count}</div>
+      <button onClick={onClickCounter}>카운트(+1)</button>
+      <button onClick={onClickMove}>이동하기</button>
+    </>
+  );
+}
