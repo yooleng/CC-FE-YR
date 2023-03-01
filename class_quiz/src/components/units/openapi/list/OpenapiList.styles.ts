76f6a3de862bb0a1cbf240b5a71c889ad5ecbba5
yrk721 <yoorae0721@naver.com>
2023-03-02 05:43:24 +0900

    16. 3-open-api - DOG API 9개 배열 - styles

---
+++ class_quiz/src/components/units/openapi/list/OpenapiList.styles.ts
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------------------------
+// day 16. 3-open-api - DOG API 9개 배열 - styles
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  border: 2px solid #1b1b1b7e;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px;
+`;
+
+export const DogImg = styled.img`
+  width: 150px;
+  height: 150px;
+`;
