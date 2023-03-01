a59e55d29a1d8573d65cc2ed3dc0a36ae5aece5b
yrk721 <yoorae0721@naver.com>
2023-03-02 05:42:45 +0900

    3-open-api (axios로 rest기반 open-api 사용하기) - 수정

---
+++ class_quiz/pages/16/3-open-api/02/index.tsx
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------------------
+// day 16. 3-open-api (axios로 rest기반 open-api 사용하기) - DOG API 9개 배열
+// ----------------------------------------------------------------------------------
+
+import OpenapiList from "../../../../src/components/units/openapi/list/OpenapiList.container";
+
+export default function OpenapisPage() {
+  return <OpenapiList />;
+}
