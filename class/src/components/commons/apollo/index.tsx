c712dd9be6e862044765762ee881dbe1ae61622d
yrk721 <yoorae0721@naver.com>
2023-03-07 03:26:48 +0900

    필요한 부분에서만 firebase를 불러와서 사용할 수 있도록 분리

--- class/src/components/commons/apollo/index.tsx
+++ class/src/components/commons/apollo/index.tsx
@@ -1,16 +1,27 @@
 // ----------------------------------------------------------------------------------
-// Apollo (graphql 설정)
+//  Apollo (graphql 설정)
 // ----------------------------------------------------------------------------------
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  ApolloLink,
+} from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
 
 interface IApolloSettingProps {
   children: JSX.Element;
 }
 
 export default function ApolloSetting(props: IApolloSettingProps) {
-  const client = new ApolloClient({
+  const uploadLink = createUploadLink({
+    // 이 uri로 업로드 기능이 가능하도록 변경해준 것
     uri: "http://backendonline.codebootcamp.co.kr/graphql",
+  });
+
+  const client = new ApolloClient({
+    link: ApolloLink.from([uploadLink as unknown as ApolloLink]), // 위의 업로드 기능을 link로 가져온 것
     cache: new InMemoryCache(), // 나중에 할 것
   });
 
