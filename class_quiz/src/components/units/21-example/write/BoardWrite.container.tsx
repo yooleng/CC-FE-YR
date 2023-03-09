2ae1d896add6dd8aa24b30d53b3eeacee3661c61
yrk721 <yoorae0721@naver.com>
2023-03-09 10:35:51 +0900

    day 21_02. Recoil - BoardWrite.container

---
+++ class_quiz/src/components/units/21-example/write/BoardWrite.container.tsx
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------------------
+//  day 21_02. Recoil - BoardWrite.container
+// ----------------------------------------------------------------------------------
+
+import BoardWriteUI from "./BoardWrite.presenter";
+
+export default function BoardWrite(props: any) {
+  return <BoardWriteUI />;
+}
