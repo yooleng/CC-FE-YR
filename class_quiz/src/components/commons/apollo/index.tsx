89d06453425e73c53cd94a2fbf4aacbc4f9819c5
yrk721 <yoorae0721@naver.com>
2023-03-09 10:32:56 +0900

    GLOBAL_STATE 생성

--- class_quiz/src/components/commons/apollo/index.tsx
+++ class_quiz/src/components/commons/apollo/index.tsx
@@ -10,6 +10,7 @@ import {
 } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 
+const GLOBAL_STATE = new InMemoryCache(); // globalState!
 interface IApolloSettingProps {
   children: JSX.Element;
 }
@@ -22,7 +23,7 @@ export default function ApolloSetting(props: IApolloSettingProps) {
 
   const client = new ApolloClient({
     link: ApolloLink.from([uploadLink as unknown as ApolloLink]), // 위의 업로드 기능을 link로 가져온 것
-    cache: new InMemoryCache(), // 나중에 할 것
+    cache: GLOBAL_STATE, // 페이지 전환(_app.tsx 리렌더)되어도, 캐시 유지
   });
 
   // prettier-ignore
