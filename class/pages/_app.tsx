e2b06581e8b39ec5cafe45c579d42216d49bb607
yrk721 <yoorae0721@naver.com>
2023-03-09 10:24:42 +0900

    Update _app.tsx - RecoilRoot 추가

--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -3,21 +3,24 @@ import Layout from "../src/components/commons/layout";
 import ApolloSetting from "../src/components/commons/apollo";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../src/commons/styles/globalStyles";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // 모든 페이지에서 useMutation을 사용하기 위한 설정
   // ApolloClient 공식 홈페이지에 가면 설정방법 메뉴얼이 있음
 
   return (
-    <ApolloSetting>
-      <>
-        <Global styles={globalStyles} />
-        <div>### app.js ###</div>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </>
-    </ApolloSetting>
+    <RecoilRoot>
+      <ApolloSetting>
+        <>
+          <Global styles={globalStyles} />
+          <div>### app.js ###</div>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </>
+      </ApolloSetting>
+    </RecoilRoot>
   );
 }
 
