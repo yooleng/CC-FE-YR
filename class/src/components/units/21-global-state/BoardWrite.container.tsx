1752d55d6c52946de753962b388704a76009d83e
yrk721 <yoorae0721@naver.com>
2023-03-09 10:27:22 +0900

    21-06. global-state - BoardWrite.container

---
+++ class/src/components/units/21-global-state/BoardWrite.container.tsx
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------------------
+//  21-06. global-state - BoardWrite.container
+// ----------------------------------------------------------------------------------
+
+import BoardWriteUI from "./BoardWrite.presenter";
+
+export default function BoardWrite() {
+  return <BoardWriteUI />;
+}
