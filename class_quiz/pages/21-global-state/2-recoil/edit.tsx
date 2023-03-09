ce36f51ab20233b8fe2458812d3a5903367473b4
yrk721 <yoorae0721@naver.com>
2023-03-09 10:34:42 +0900

    global-state - 02. Recoil 적용하기 - edit page

---
+++ class_quiz/pages/21-global-state/2-recoil/edit.tsx
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------------------------
+//  day 21. global-state
+//  02. Recoil 적용하기 - edit page
+// ----------------------------------------------------------------------------------
+
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { isEditState } from "../../../src/components/commons/store";
+import BoardWrite from "../../../src/components/units/21-example/write/BoardWrite.container";
+
+export default function GlobalStatePage() {
+  //   const [isEdit, setIsEdit] = useState(false);
+  const [isEdit, setIsEdit] = useRecoilState(isEditState);
+
+  useEffect(() => {
+    setIsEdit(true);
+  }, []);
+
+  return <BoardWrite isEdit={true} />;
+}
