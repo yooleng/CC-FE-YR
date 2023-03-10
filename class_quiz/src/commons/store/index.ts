0e7ba946229d2b81ce4d49ba4bf3d840b85b8b14
yrk721 <yoorae0721@naver.com>
2023-03-10 09:19:15 +0900

    store에 accessTokenState 추가

---
+++ class_quiz/src/commons/store/index.ts
@@ -0,0 +1,15 @@
+// ----------------------------------------------------------------------------------
+//  day 21~22. global-state - store
+// ----------------------------------------------------------------------------------
+
+import { atom } from "recoil";
+
+export const isEditState = atom({
+  key: "isEditState",
+  default: false,
+});
+
+export const accessTokenState = atom({
+  key: "accessTokenState",
+  default: "",
+});
