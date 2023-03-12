2da4f910d9b172c06e3665cc03328263f678532b
yrk721 <yoorae0721@naver.com>
2023-03-13 08:15:25 +0900

    Update _app.tsx

--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -5,7 +5,7 @@ import { Global } from "@emotion/react";
 import { globalStyles } from "../src/commons/styles/globalStyles";
 import { RecoilRoot } from "recoil";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component }: AppProps) {
   // 모든 페이지에서 useMutation을 사용하기 위한 설정
   // ApolloClient 공식 홈페이지에 가면 설정방법 메뉴얼이 있음
 
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Global styles={globalStyles} />
           <div>### app.js ###</div>
           <Layout>
-            <Component {...pageProps} />
+            <Component />
           </Layout>
         </>
       </ApolloSetting>
