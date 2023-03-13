5c8223bb1973ba0351869c3c0d83ed90b20ba3fe
yrk721 <yoorae0721@naver.com>
2023-03-13 21:23:00 +0900

    24-02. react-hook-form / 04-04 compare

---
+++ class/pages/24-02-react-hook-form/04-04-compare.js
@@ -0,0 +1,36 @@
+// ----------------------------------------------------------------------------------
+// 04-04. graphql-mutation-input
+// ----------------------------------------------------------------------------------
+
+import { useState } from "react";
+
+export default function GraphqlMutationPage() {
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const onClickSubmit = async () => {
+    console.log(writer, title, contents);
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  return (
+    <>
+      작성자: <input type="text" onChange={onChangeWriter} />
+      제목: <input type="text" onChange={onChangeTitle} />
+      내용: <input type="text" onChange={onChangeContents} />
+      <button onClick={onClickSubmit}>GRAPHQL-API(동기) 요청하기</button>
+    </>
+  );
+}
