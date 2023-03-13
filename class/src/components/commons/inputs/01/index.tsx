e459a7ec441ba85642bafb3940d33282a2896ae7
yrk721 <yoorae0721@naver.com>
2023-03-13 21:23:29 +0900

    24-04. common-components - inputs/01

---
+++ class/src/components/commons/inputs/01/index.tsx
@@ -0,0 +1,14 @@
+// ----------------------------------------------------------------------------------
+//  24-04. common-components - inputs/01
+// ----------------------------------------------------------------------------------
+
+import { UseFormRegisterReturn } from "react-hook-form";
+
+interface IProps {
+  type: "text" | "password";
+  register: UseFormRegisterReturn;
+}
+
+export default function Input01(props: IProps) {
+  return <input type={props.type} {...props.register} />;
+}
