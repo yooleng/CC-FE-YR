5b8a4da1a6d8e9d88bd72ee26340469f1767cfe0
yrk721 <yoorae0721@naver.com>
2023-03-13 12:00:44 +0900

    Create withAuth.tsx - router 경로 수정

---
+++ class_quiz/src/components/commons/hocs/withAuth.tsx
@@ -0,0 +1,19 @@
+// ----------------------------------------------------------------------------------
+//  day 23. HOF - withAuth
+// ----------------------------------------------------------------------------------
+
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export const withAuth = (Component: any) => (props: any) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!localStorage.getItem("accessToken")) {
+      alert("로그인 후 이용 가능합니다!");
+      void router.push("/23/02-login");
+    }
+  }, []);
+
+  return <Component {...props} />;
+};
