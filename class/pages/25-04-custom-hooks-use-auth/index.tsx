72fecb43f78186675f69a291bde2574c6cd00c8c
yrk721 <yoorae0721@naver.com>
2023-03-15 09:30:46 +0900

    25-04. custom-hooks-use-auth

---
+++ class/pages/25-04-custom-hooks-use-auth/index.tsx
@@ -0,0 +1,11 @@
+// ----------------------------------------------------------------------------------
+//  25-04. custom-hooks-use-auth
+// ----------------------------------------------------------------------------------
+
+import { useAuth } from "../../src/components/commons/hooks/useAuth";
+
+export default function CustomHooksUseAuthPage() {
+  useAuth();
+
+  return <div>프로필 페이지입니다.</div>;
+}
