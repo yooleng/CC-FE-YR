4e4eadb68d527e8f85d4c71795fe77af303423e1
yrk721 <yoorae0721@naver.com>
2023-03-16 13:46:51 +0900

    [#day 26] 26-01. kakao-map

---
+++ class/pages/26-01-kakao-map/index.tsx
@@ -0,0 +1,36 @@
+// ---------------------------------------------------------------------------------------
+//  26-01. kakao-map
+// ---------------------------------------------------------------------------------------
+
+import Head from "next/head";
+import { useEffect } from "react";
+
+declare const window: typeof globalThis & {
+  kakao: any;
+};
+
+export default function KakaoMapPage() {
+  useEffect(() => {
+    const container = document.getElementById("map"); // 지도를 담을 영역의 DOM 레퍼런스
+    const options = {
+      // 지도를 생성할 때 필요한 기본 옵션
+      center: new window.kakao.maps.LatLng(37.550963, 126.941038), // 지도의 중심좌표.
+      level: 3, // 지도의 레벨(확대, 축소 정도)
+    };
+
+    const map = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+    console.log(map);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <script
+          type="text/javascript"
+          src="//dapi.kakao.com/v2/maps/sdk.js?appkey=46eccc28df26f5b1d770fd6d6762717d"
+        ></script>
+      </Head>
+      <div id="map" style={{ width: 500, height: 400 }}></div>
+    </>
+  );
+}
