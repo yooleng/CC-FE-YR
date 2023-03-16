213ce6d1265df4337820dc0d231e4f283bb5737c
yrk721 <yoorae0721@naver.com>
2023-03-16 13:47:04 +0900

    26-04. apollo-cache-state

---
+++ class/pages/26-04-apollo-cache-state/index.tsx
@@ -0,0 +1,108 @@
+// ----------------------------------------------------------------------------------
+//  26-04. apollo-cache-state
+// ----------------------------------------------------------------------------------
+
+import { useQuery, gql, useMutation } from "@apollo/client";
+import {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../src/commons/types/generated/types";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+const DELETE_BOARD = gql`
+  mutation deleteBoard($boardId: ID!) {
+    deleteBoard(boardId: $boardId)
+  }
+`;
+
+// 캐시에 저장되는 데이터와 요청 후 받아오는 값이 일치해야 한다.
+const CREATE_BOARD = gql`
+  mutation createBoard($createBoardInput: CreateBoardInput!) {
+    createBoard(createBoardInput: $createBoardInput) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutedPage() {
+  const { data } = useQuery<Pick<IQuery, "fetchBoards">, IQueryFetchBoardsArgs>(
+    FETCH_BOARDS
+  );
+  console.log("data", data?.fetchBoards);
+
+  const [deleteBoard] = useMutation(DELETE_BOARD);
+  const [createBoard] = useMutation(CREATE_BOARD);
+
+  const onClickDelete = (boardId: string) => () => {
+    void deleteBoard({
+      variables: { boardId },
+      // refetchQueries: [{ query: FETCH_BOARDS }], // 예전 방식
+      update(cache, { data }) {
+        // cache : apollo-cache-state (global-state)에 있는 data
+        // {data} : 삭제하고 나서 받은 결과
+        cache.modify({
+          fields: {
+            fetchBoards: (prev, { readField }) => {
+              // prev에서 key값을 읽고 싶을 때에는 readField 사용
+              const deletedId = data.deleteBoard; // 삭제된 ID
+              const filteredPrev = prev.filter(
+                (el) => readField("_id", el) !== deletedId // el._id가 안되므로, readField를 사용해서 꺼내오기
+              );
+              return [...filteredPrev]; // 삭제된 ID를 제외한 나머지 9개만 리턴
+            },
+          },
+        });
+      },
+    });
+  };
+
+  const onClickCreate = () => {
+    void createBoard({
+      variables: {
+        createBoardInput: {
+          writer: "철쑤",
+          password: "11",
+          title: "제목이고",
+          contents: "내용임~!",
+        },
+      },
+      // refetchQueries: [{query: FETCH_BOARDS}] // 예전 방식
+
+      update(cache, { data }) {
+        cache.modify({
+          fields: {
+            fetchBoards: (prev) => {
+              return [data.createBoard, ...prev];
+            },
+          },
+        });
+      },
+    });
+  };
+
+  return (
+    <>
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span style={{ margin: "10px" }}>{el.writer} </span>
+          <span style={{ margin: "10px" }}>{el.title}</span>
+          <button onClick={onClickDelete(el._id)}>삭제하기</button>
+        </div>
+      ))}
+      <button onClick={onClickCreate}>등록하기</button>
+    </>
+  );
+}
