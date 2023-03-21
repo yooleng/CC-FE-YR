b0d0e800e1a6da0a59e8b9fe903547babcbc0ce0
yrk721 <yoorae0721@naver.com>
2023-03-22 08:56:51 +0900

    Create getAccessToken.ts

---
+++ class/src/commons/libraries/getAccessToken.ts
@@ -0,0 +1,28 @@
+// --------------------------------------------------------------------------------
+//  getAccessToken
+// --------------------------------------------------------------------------------
+
+import { gql, GraphQLClient } from "graphql-request";
+
+const RESTORE_ACCESS_TOKEN = gql`
+mutation restoreAccessToken {
+  restoreAccessToken () {
+    accessToken
+  }
+}
+`;
+
+export const getAccessToken = async () => {
+  // 2-1. refreshToken으로 accessToken을 재발급 받기
+  try {
+    const graphQLClient = new GraphQLClient(
+      "https://backendonline.codebootcamp.co.kr/graphql",
+      { credentials: "include" }
+    );
+    const result = await graphQLClient.request(RESTORE_ACCESS_TOKEN);
+    const newAccessToken = result.restoreAccessToken.accessToken;
+    return newAccessToken;
+  } catch (error) {
+    if (error instanceof Error) console.log(error.message);
+  }
+};
