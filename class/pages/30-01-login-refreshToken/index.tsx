53a70fefed442becbb48ca52a7d9b489e2da05f6
yrk721 <yoorae0721@naver.com>
2023-03-22 08:56:07 +0900

    30-01. login-refreshToken

---
+++ class/pages/30-01-login-refreshToken/index.tsx
@@ -0,0 +1,77 @@
+// ----------------------------------------------------------------------------------
+//  30-01. login-refreshToken
+// ----------------------------------------------------------------------------------
+
+import { gql, useMutation } from "@apollo/client";
+import { ChangeEvent, useState } from "react";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../../src/commons/store";
+import { useRouter } from "next/router";
+import { Modal } from "antd";
+import {
+  IMutation,
+  IMutationLoginUserExampleArgs,
+} from "../../src/commons/types/generated/types";
+
+// loginUserExample : 로그인 만료시간 5초인 test api
+const LOGIN_USER_EXAMPLE = gql`
+  mutation loginUserExample($email: String!, $password: String!) {
+    loginUserExample(email: $email, password: $password) {
+      accessToken
+      __typename
+    }
+  }
+`;
+
+export default function LoginPage() {
+  const router = useRouter();
+
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loginUserExample] = useMutation<
+    Pick<IMutation, "loginUserExample">,
+    IMutationLoginUserExampleArgs
+  >(LOGIN_USER_EXAMPLE);
+
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const onChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const onClickLogin = async () => {
+    try {
+      // 1. 로그인해서 accessToken(JWT 토큰)받아오기
+      const result = await loginUserExample({
+        variables: { email, password },
+      });
+      const accessToken = result.data?.loginUserExample.accessToken;
+      console.log("accessToken", accessToken);
+
+      // 2. accessToken을 globalState에 저장하기
+      if (!accessToken) {
+        Modal.error({ content: "로그인에 실패하였습니다. 다시 시도해주세요." });
+        return;
+      }
+      setAccessToken(accessToken);
+
+      // 3. 로그인 성공 페이지로 이동하기
+      void router.push("/30-02-login-refreshToken-success");
+    } catch (error) {
+      if (error instanceof Error) Modal.error({ content: error.message });
+    }
+  };
+
+  return (
+    <>
+      이메일 : <input type="text" onChange={onChangeEmail} />
+      <br />
+      비밀번호 : <input type="password" onChange={onChangePassword} />
+      <button onClick={onClickLogin}>로그인하기</button>
+    </>
+  );
+}
