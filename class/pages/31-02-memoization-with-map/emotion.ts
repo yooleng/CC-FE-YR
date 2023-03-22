1610c9ef07c669a26a36cff6efb5133c21cc040f
yrk721 <yoorae0721@naver.com>
2023-03-23 08:56:02 +0900

    31-02. memoization-with-map - emotion

---
+++ class/pages/31-02-memoization-with-map/emotion.ts
@@ -0,0 +1,13 @@
+// ----------------------------------------------------------------------------------
+//  31-02. memoization-with-map - emotion
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+export const PurpleTag = styled.span`
+  color: #b01bd5;
+`;
+
+export const BlueTag = styled.span`
+  color: #2c63da;
+`;
