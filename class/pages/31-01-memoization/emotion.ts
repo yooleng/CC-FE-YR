d6dfaf0dbd1159380ac998e0f11bb2260c0d74d6
yrk721 <yoorae0721@naver.com>
2023-03-23 08:54:10 +0900

    31-01. memoization - emotion

---
+++ class/pages/31-01-memoization/emotion.ts
@@ -0,0 +1,13 @@
+// ----------------------------------------------------------------------------------
+//  31-01. memoization - emotion
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+export const PurpleTag = styled.span`
+  color: #b01bd5;
+`;
+
+export const BlueTag = styled.span`
+  color: #2c63da;
+`;
