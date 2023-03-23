be123e9b46b1f66b599ca29a4da9de49c762f43e
yrk721 <yoorae0721@naver.com>
2023-03-23 09:00:42 +0900

    31. memoization - emotion

---
+++ class_quiz/pages/31/emotion.ts
@@ -0,0 +1,17 @@
+// ----------------------------------------------------------------------------------
+//  day 31. memoization - emotion
+// ----------------------------------------------------------------------------------
+
+import styled from "@emotion/styled";
+
+export const PurpleTag = styled.span`
+  color: #b01bd5;
+`;
+
+export const BlueTag = styled.span`
+  color: #2c63da;
+`;
+
+export const ContentsBox = styled.div`
+  margin: 15px;
+`;
