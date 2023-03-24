3890c266f1707c6e4835c8c2c6d6b61017966b60
yrk721 <yoorae0721@naver.com>
2023-03-24 09:45:20 +0900

    32-08. data-prefetch-moved / boards - edit (수정 페이지)

---
+++ class/pages/32-08-data-prefetch-moved/[boardId]/edit-x/index.js
@@ -0,0 +1,32 @@
+// ----------------------------------------------------------------------------------
+//  32-08. data-prefetch-moved / boards - edit (수정 페이지)
+// ----------------------------------------------------------------------------------
+
+import BoardWriteComponent from "@/src/components/units/board/09-write/BoardWrite.container";
+import { gql, useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
+
+const FETCH_BOARD = gql`
+  # graphql 내용 등록
+  query fetchBoard($number: Int) {
+    fetchBoard(number: $number) {
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function BoardsEditPage() {
+  const router = useRouter();
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: {
+      number: Number(router.query.number),
+    },
+  });
+  return (
+    <>
+      <BoardWriteComponent isEdit={true} data={data} />
+    </>
+  );
+}
